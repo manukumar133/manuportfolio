1ee8bec32b34ccff2b4da791d0e44149e3dc47bf
manukumar133 <81145349+manukumar133@users.noreply.github.com>
2024-06-30 11:03:28 +0530

    Add files via upload

---
+++ table.java
@@ -0,0 +1,12 @@
+import java.util.*; 
+class table{
+    public static void main(String[] args){
+        
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        system.out.println("Pls enter a number here");
+        for(int i=1;i<=10;i++){
+            System.out.println(i*n);
+        }
+    }
+}
